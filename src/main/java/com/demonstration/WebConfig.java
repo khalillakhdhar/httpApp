93f13ee95c196dd5aa6268827a354ad6678dec4b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-14 15:45:16 +0100

    update

--- src/main/java/com/demonstration/WebConfig.java
+++ src/main/java/com/demonstration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Vous pouvez spécifier ici l'URL spécifique à laquelle vous souhaitez autoriser l'accès.
-            .allowedOrigins("http://localhost:4200") // Autoriser l'accès depuis ce domaine Angular.
+            .allowedOrigins("**") // Autoriser l'accès depuis ce domaine Angular.
             .allowedMethods("GET", "POST", "PUT", "DELETE") // Méthodes HTTP autorisées.
             .allowedHeaders("*"); // Tous les en-têtes autorisés.
     }
--- src/main/java/com/demonstration/model/Audit.java
+++ src/main/java/com/demonstration/model/Audit.java
@@ -12,4 +12,15 @@ public class Audit {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	
 }
